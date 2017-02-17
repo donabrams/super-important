8b248de13f8ba8c7cd0c291c7c03be6526b6b4d7
Don <don.abrams@smartrac-group.com>
2017-02-16 22:57:11 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var express = require("express")
+var getImages = require("google-image-search-url-results")
+var imgToAscii = require("image-to-ascii")
+
+var app = express()
+
+app.get(/.*/, (req, res) => {
+	var keywords = req.url.split("/").join(" ").trim()
+	if (keywords === "favicon.ico") {
+		res.send("no")
+		return
+	}
+	getImages(keywords)
+		.then((images) => {
+			return Promise.all(images.map((src) => {
+				return new Promise((resolve, reject)=> {
+					imgToAscii(src, {
+						colored: false, 
+						reverse: true,
+						size_options: {
+							screen_size: {
+								width: 50,
+								height: 50
+							}
+						}
+					}, (err, converted)=>{
+						err ? reject(err) : resolve(converted)
+					})
+				})
+			}))
+		})
+		.then((artz)=> {
+			res.type("text/plain")
+			res.send(artz.join("\n\n"))
+		})
+		.catch((err)=> {
+			console.log(err)
+			res.send("SUPER SAD")
+		})
+
+})
+
+app.listen(8000, () => {
+	console.log("super-important stuff up at localhost:8000")
+})
